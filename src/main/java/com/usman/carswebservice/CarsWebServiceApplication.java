95d393a7ce38f4bfc7f723c530681bd31aea69e3
muak4 <muak@Arbisofts-MacBook-Pro.local>
2024-03-23 04:15:37 +0500

    Integrate Swagger API Docs

--- src/main/java/com/usman/carswebservice/CarsWebServiceApplication.java
+++ src/main/java/com/usman/carswebservice/CarsWebServiceApplication.java
@@ -1,11 +1,14 @@
 package com.usman.carswebservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@RestController
+@OpenAPIDefinition(info = @Info(title = "Cars Web Service", version = "1.0", description = "Spring Boot APIs For Car Backend Service"))
 public class CarsWebServiceApplication {
 
 	public static void main(String[] args) {
